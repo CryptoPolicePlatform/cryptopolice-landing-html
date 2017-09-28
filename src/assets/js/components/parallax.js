be8b02d63d80d5c71c7b373b25e2bb8215fab756
Dmitrijs Valaks <dmitry.valak@gmail.com>
2017-09-28 23:22:33 +0200

    some new blocks

---
+++ src/assets/js/components/parallax.js
@@ -0,0 +1,11 @@
+$('.js_parallax_big').parallax({
+    imageSrc: 'assets/i/parallax-big.png',
+    positionY: 'top'
+});
+
+
+$('.js_parallax_small').parallax({
+    imageSrc: 'assets/i/parallax-small.png',
+    positionY: 'top',
+    speed: 0.5
+});
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,8 @@
 //= ../../../bower_components/jquery/dist/jquery.js
 //= ../../../bower_components/particles.js/particles.js
+//= ../../../bower_components/parallax.js/parallax.js
 
 //= components/tabs.js
 //= components/particles.js
+//= components/parallax.js
 
