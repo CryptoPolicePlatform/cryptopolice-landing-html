240d911e08962cd374cf8b1c1e36fa5f8ad95ca0
Dmitrijs Valaks <dmitry.valak@gmail.com>
2017-09-29 21:24:52 +0200

    new blocks

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,6 @@
 //= ../../../bower_components/jquery/dist/jquery.js
 //= ../../../bower_components/particles.js/particles.js
-//= ../../../bower_components/parallax.js/parallax.js
+//= ../../../bower_components/parallax-background/js/parallax-background.js
 //= ../../../bower_components/wow/dist/wow.js
 
 //= components/tabs.js
