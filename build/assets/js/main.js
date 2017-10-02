a523571d462ca545dd631c09ff237c87e7757925
Dmitrijs Valaks <dmitry.valak@gmail.com>
2017-10-02 11:05:23 +0200

    changes

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -12592,12 +12592,12 @@ $(function() {
 
 });
 "use strict";
-
+/*
 particlesJS.load('particles-top-left', 'assets/particles-top-left.json', function() {});
 particlesJS.load('particles-bottom-left', 'assets/particles-top-left.json', function() {});
 
 particlesJS.load('particles-top-right', 'assets/particles-top-right.json', function() {});
-particlesJS.load('particles-bottom-right', 'assets/particles-top-right.json', function() {});
+particlesJS.load('particles-bottom-right', 'assets/particles-top-right.json', function() {});*/
 $('.officer_parallax_big').parallaxBackground();
 $('.officer_parallax_small').parallaxBackground();
 
