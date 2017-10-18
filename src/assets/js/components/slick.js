c6857515a3129aa346ddc8d6e9a19272222faeba
Dmitrijs Valaks <dmitry.valak@gmail.com>
2017-10-18 12:53:20 +0300

    team slider

--- src/assets/js/components/slick.js
+++ src/assets/js/components/slick.js
@@ -9,54 +9,34 @@ $(function() {
     * Docs: https://github.com/kenwheeler/slick
     */
 
-    var slider_1 = $("#js-slider-1");
+    var team_slider = $("#team_slider"),
+        team_info = $("#team_info");
 
-    if(slider_1.length > 0) {
-        slider_1.slick({
-            infinite: true,
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 4000,
-            dots: false,
-            responsive: [
-                {
-                    breakpoint: 991,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        });
-    }
+    team_slider.slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        variableWidth: true,
+        centerMode: true,
+        centerPadding: "0",
+        focusOnSelect: true,
+        swipeToSlide: true,
+        initialSlide: 4,
+        asNavFor: "#team_info"
+    });
 
-
-    var slider_2 = $("#js-slider-2");
-
-    if(slider_2.length > 0) {
-        slider_2.slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            autoplay: false,
-            dots: true,
-            responsive: [
-                {
-                    breakpoint: 991,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
-        });
-    }
+    team_info.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        asNavFor: "#team_slider",
+        swipe: false,
+        fade: true,
+        initialSlide: 4
+    });
 
 });
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,10 +2,13 @@
 //= ../../../bower_components/particles.js/particles.js
 //= ../../../bower_components/parallax-background/js/parallax-background.js
 //= ../../../bower_components/wow/dist/wow.js
+//= ../../../bower_components/slick-carousel/slick/slick.js
+//= ../../../node_modules/swiper/dist/js/swiper.js
 
 //= components/tabs.js
 //= components/particles.js
 //= components/parallax.js
 //= components/wow.js
 //= components/scroll.js
+//= components/slick.js
 
