45914fadf288297ec0185b636bb62174765400fa
Dmitrijs Valaks <dmitry.valak@gmail.com>
2017-10-27 13:16:06 +0300

    fix

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -22756,11 +22756,11 @@ $(function() {
 });
 "use strict";
 
-/*particlesJS.load('particles-top-left', 'assets/particles-top-left.json', function() {});
+particlesJS.load('particles-top-left', 'assets/particles-top-left.json', function() {});
 particlesJS.load('particles-bottom-left', 'assets/particles-top-left.json', function() {});
 
 particlesJS.load('particles-top-right', 'assets/particles-top-right.json', function() {});
-particlesJS.load('particles-bottom-right', 'assets/particles-top-right.json', function() {});*/
+particlesJS.load('particles-bottom-right', 'assets/particles-top-right.json', function() {});
 $('.officer_parallax_big').parallaxBackground();
 $('.officer_parallax_small').parallaxBackground();
 
