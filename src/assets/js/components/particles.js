2dde521725ac3de07f10fac7b64e562ec30c2129
Dmitrijs Valaks <dmitry.valak@gmail.com>
2017-11-03 15:29:57 +0200

    changes: new intro, new blocks

--- src/assets/js/components/particles.js
+++ src/assets/js/components/particles.js
@@ -1,7 +1,7 @@
 "use strict";
 
-particlesJS.load('particles-top-left', 'assets/particles-top-left.json', function() {});
+/*particlesJS.load('particles-top-left', 'assets/particles-top-left.json', function() {});
 particlesJS.load('particles-bottom-left', 'assets/particles-top-left.json', function() {});
 
 particlesJS.load('particles-top-right', 'assets/particles-top-right.json', function() {});
-particlesJS.load('particles-bottom-right', 'assets/particles-top-right.json', function() {});
+particlesJS.load('particles-bottom-right', 'assets/particles-top-right.json', function() {});*/
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,9 @@
 //= ../../../bower_components/jquery/dist/jquery.js
-//= ../../../bower_components/particles.js/particles.js
 //= ../../../bower_components/parallax-background/js/parallax-background.js
 //= ../../../bower_components/wow/dist/wow.js
 //= ../../../bower_components/slick-carousel/slick/slick.js
-//= ../../../node_modules/swiper/dist/js/swiper.js
 
 //= components/tabs.js
-//= components/particles.js
 //= components/parallax.js
 //= components/wow.js
 //= components/scroll.js
