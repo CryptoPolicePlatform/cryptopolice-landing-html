abbdb7f3747abd1ebdfdf44f137ba463cce76b38
Unknown <e.teiks@gmail.com>
2017-11-20 16:40:51 +0200

    new build version

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -22222,3 +22222,43 @@ $(function() {
     });
 
 });
+$.ajaxSetup({
+    contentType: "application/json"
+});
+$(function () {
+    "use strict";
+
+    $('form[data-js-subscribe]').submit(function (event) {
+        event.preventDefault();
+        var form = $(this);
+        $.post("https://api.cryptopolice.io/api/subscribe", {
+            Name: form.find('[name=Name]').val(),
+            Email: form.find('[name=Email]').val(),
+        }).done(function () {
+
+        }).fail(function () {
+            
+        })
+    })
+})
+$(function () {
+    "use strict";
+
+    $('form[data-ajax-officer-registration]').submit(function (event) {
+        event.preventDefault();
+        var form = $(this);
+        $.post("https://api.cryptopolice.io/api/officer-signup", {
+            Name: form.find('[name=Name]').val(),
+            Nickame: form.find('[name=Nickame]').val(),
+            Email: form.find('[name=Email]').val(),
+            Country: form.find('[name=Country]').val(),
+            YearsCryptoWorldExperience: form.find('[name=YearsCryptoWorldExperience]').val(),
+            BlockchainKnowledgePercentage: form.find('[name=BlockchainKnowledgePercentage]').val(),
+            TradingKnowledgePercentage: form.find('[name=TradingKnowledgePercentage]').val(),
+        }).done(function () {
+
+        }).fail(function () {
+            
+        })
+    })
+})
