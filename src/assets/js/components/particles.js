046dfc6a3948d362451753a24600f511b2872722
Dmitrijs Valaks <dmitry.valak@cunami.lv>
2018-02-27 20:54:53 +0200

    contact icons

--- src/assets/js/components/particles.js
+++ src/assets/js/components/particles.js
@@ -2,10 +2,10 @@ $(function() {
 
     var windowW = $(window).innerWidth();
 
-    if(windowW > 600 && $("#particles-bottom-left").length && $("#particles-bottom-right").length) {
+    /*if(windowW > 600 && $("#particles-bottom-left").length && $("#particles-bottom-right").length) {
         particlesJS.load('particles-bottom-left', 'assets/particles-top-left.json', function() {});
         particlesJS.load('particles-bottom-right', 'assets/particles-top-right.json', function() {});
-    }
+    }*/
 
 });
 
