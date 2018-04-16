24ecd34798ca9362163563ba0cedf90374b82750
Elvijs Teikmanis <ete@cunami.lv>
2018-04-16 18:14:13 +0300

    updated helpers

--- src/assets/js/components/helpers.js
+++ src/assets/js/components/helpers.js
@@ -1,16 +1,30 @@
 window.appApiHost = function (uri, v) {
     return "https://api.cryptopolice.com/api" + (v ? "/" + v : "") + uri 
-}
+};
 
 window.appFormHelpers = {
-    getJson: function ($form, fieldsArray) {
-        var obj = {};
+    getJson: function ($form, options, obj) {
+        if ( ! obj) {
+            obj = {};
+        }
 
-        for (var field in fieldsArray) {
-            var val = $form.find("[name=" + fieldsArray[field] + "]").val();
-            if (val) {
-                obj[fieldsArray[field]] = val;
+        if (options instanceof Array) {
+            for (var field in options) {
+                var val = $form.find("[name=" + options[field] + "]").val();
+                if (val) {
+                    obj[options[field]] = val;
+                }
             }
+        } else {
+            $form.find(options).find(':input').each(function () {
+                var $el = $(this);
+                var val = $el.val();
+                var name = $el.attr("name");
+
+                if (name && val) {
+                    obj[$el.attr("name")] = val;
+                }
+            })
         }
 
         return JSON.stringify(obj);
@@ -20,4 +34,11 @@ window.appFormHelpers = {
             'X-G-Recaptcha-Response': $form.find('[name=g-recaptcha-response]').val()
         }
     }
-}
+};
+
+window.getUrlParameter = function (name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
