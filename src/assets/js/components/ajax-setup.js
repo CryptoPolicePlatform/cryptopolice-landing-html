78de9dfa3d2507bacbd469a47fb40bf56ddf64aa
Elvijs Teikmanis <ete@cunami.lv>
2018-04-16 18:20:15 +0300

    refactoring

--- src/assets/js/components/ajax-setup.js
+++ src/assets/js/components/ajax-setup.js
@@ -1,29 +1,3 @@
 $.ajaxSetup({
-    contentType: "application/json",
-    error: function ($xhr) {
-        if ($xhr.responseJSON) {
-            var messages = [];
-            
-            var error = $xhr.responseJSON.error;
-
-            if (error && error.message)
-            {
-                messages.push(error.message);
-
-                if (error.details instanceof Array)
-                {
-                    error.details.forEach(function (detail) {
-                        if (detail.message) {
-                            messages.push(detail.message);
-                        }
-                    });
-                }
-
-                showAppAlert('error', messages);
-                return;
-            }
-        }
-
-        showAppAlert('error', ['Sorry, something went wrong. Please try again later.']);
-    }
+    contentType: "application/json"
 });
--- src/assets/js/components/helpers.js
+++ src/assets/js/components/helpers.js
@@ -42,3 +42,30 @@ window.getUrlParameter = function (name) {
     var results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
+
+window.jqXhrErrorHandler = function ($xhr) {
+    if ($xhr.responseJSON) {
+        var messages = [];
+        
+        var error = $xhr.responseJSON.error;
+
+        if (error && error.message)
+        {
+            messages.push(error.message);
+
+            if (error.details instanceof Array)
+            {
+                error.details.forEach(function (detail) {
+                    if (detail.message) {
+                        messages.push(detail.message);
+                    }
+                });
+            }
+
+            showAppAlert('error', messages);
+            return;
+        }
+    }
+
+    showAppAlert('error', ['Sorry, something went wrong. Please try again later.']);
+};
