481e647eda71ade73024ce9e7d084f397a089384
ART <art@cunami.lv>
2018-05-12 03:01:22 +0300

    Timer update & new build

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -22473,7 +22473,7 @@ $(function() {
 
     "use strict";
 
-    var date = new Date(2018, 4, 15); // YYYY, 0-11 - month, 1-31 day
+    var date = new Date(2018, 5, 27); // YYYY, 0-11 - month, 1-31 day
 
     $('#timer').countdown({
         until: date,
